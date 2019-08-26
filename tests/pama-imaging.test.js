5b319df395b82b27716f661b4286121b2da7eafc
Carl Anderson <carl.anderson@microsoft.com>
2019-08-26 13:30:43 -0500

    feat: adds a skeletal unit test for the imaging service

---
+++ tests/pama-imaging.test.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+
+// const request = require('supertest');
+
+describe('PAMA Imaging Service Endpoint', () => {
+  test('it returns true', (done) => {
+    done();
+  });
+});
