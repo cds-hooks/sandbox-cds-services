a6d2b2ca7cb24519304550e6d99b0feb0131c4ae
Carl Anderson <carl.anderson@microsoft.com>
2019-08-27 17:28:56 -0500

    feat: begin to address some cardinality issues in previous version (still very much WIP)

--- services/pama-imaging.js
+++ services/pama-imaging.js
@@ -2,52 +2,55 @@ const express = require('express');
 
 const router = express.Router();
 
-
-function extractInput(request) {
-  // TODO: return a dictionary with only the important logical bits present.
-  return request;
+/**
+ * Finds a resource from a bundle matching a selection.
+ *
+ * @param {*} entries a list of resources.
+ * @param {*} selection a selected resource string matching the form 'Type/Id'.
+ */
+function findResource(entries, selection) {
+  const [resourceType, resourceId] = selection.split('/');
+  return entries
+    .map(e => e.resource)
+    .filter(r => r.resourceType === resourceType && r.id === resourceId);
 }
 
-function getResponse(pamaRating) {
-  return {
-    extension: {
-      systemActions: [{
-        resource: {
-          extension: [
-            {
-              url: 'http://fhir.org/argonaut/Extension/pama-rating',
-              valueCodeableConcept: {
-                coding: [{
-                  system: 'http://fhir.org/argonaut/CodeSystem/pama-rating',
-                  code: pamaRating,
-                }],
-              },
-            },
-            {
-              url: 'http://fhir.org/argonaut/Extension/pama-rating-qcdsm-consulted',
-              valueString: 'example-gcode',
-            },
-            {
-              url: 'http://fhir.org/argonaut/Extension/pama-rating-consult-id',
-              valueUri: 'urn:uuid:55f3b7fc-9955-420e-a460-ff284b2956e6',
-            },
-          ],
-        },
-        type: 'update',
-      }],
+function getSystemActions(entries, selections) {
+  const r = findResource(entries, selections[0]);
+  r.extension = [
+    {
+      url: 'http://fhir.org/argonaut/Extension/pama-rating',
+      valueCodeableConcept: {
+        coding: [{
+          system: 'http://fhir.org/argonaut/CodeSystem/pama-rating',
+          code: 'appropriate',
+        }],
+      },
     },
-  }
-}
-
-function determinePamaRating(input) {
-  // TODO: apply logic here.
-  return 'appropriate';
+    {
+      url: 'http://fhir.org/argonaut/Extension/pama-rating-qcdsm-consulted',
+      valueString: 'example-gcode',
+    },
+    {
+      url: 'http://fhir.org/argonaut/Extension/pama-rating-consult-id',
+      valueUri: 'urn:uuid:55f3b7fc-9955-420e-a460-ff284b2956e6',
+    },
+  ];
+  return [{
+    resource: r,
+    type: 'update',
+  }];
 }
 
 router.post('/', (request, response) => {
-  const input = extractInput(request.body);
-  const coding = determinePamaRating(input);
-  response.json(getResponse(coding));
+  const entries = request.body.context.draftOrders.entry;
+  const { selections } = request.body.context;
+  response.json({
+    cards: [],
+    extension: {
+      systemActions: getSystemActions(entries, selections),
+    },
+  });
 });
 
 module.exports = router;
