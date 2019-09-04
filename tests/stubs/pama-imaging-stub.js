5400f8b4dfe255d557a44e483b98f94a747b5a4e
Carl Anderson <carl.anderson@microsoft.com>
2019-09-04 16:39:09 -0500

    feat: function to generate messages and a new request to cover no-guidelines-apply case

--- tests/stubs/pama-imaging-stub.js
+++ tests/stubs/pama-imaging-stub.js
@@ -1,43 +1,102 @@
-module.exports = {
-  scenario1request: {
-    hook: 'order-select',
-    hookInstance: 'd1577c69-dfbe-44ad-ba6d-3e05e953b2ea',
-    fhirServer: 'http://hooks.smarthealthit.org',
+const uuid = require('uuid');
+
+function getReasons(reasons) {
+  return reasons.map(r => (
+    {
+      coding: r.reasons.map(x => (
+        {
+          system: 'http://snomed.info/sct',
+          code: x.code,
+          display: x.display,
+        }
+      )),
+    }
+  ));
+}
+
+function getResources(
+  bundle, patientId,
+  intent = 'plan',
+  resourceType = 'ServiceRequest',
+  status = 'draft',
+) {
+  return bundle.map(x => (
+    {
+      resource: {
+        resourceType,
+        id: `example-${patientId}`,
+        status,
+        intent,
+        code: {
+          coding: [{
+            system: 'http://www.ama-assn.org/go/cpt',
+            code: x.code,
+          }],
+          text: x.text,
+        },
+        subject: {
+          reference: `Patient/${patientId}`,
+        },
+        reasonCode: getReasons(x.snomed),
+      },
+    }
+  ));
+}
+
+function getRequest(
+  bundle,
+  encounterId = '89284',
+  fhirServer = 'http://hooks.smarthealthit.org',
+  hook = 'order-select',
+  patientId = 'MRI-59879846',
+  userId = 'Practitioner/123',
+) {
+  return {
+    hook,
+    hookInstance: `${uuid.v4()}`,
+    fhirServer,
     context: {
-      userId: 'Practitioner/123',
-      patientId: 'MRI-59879846',
-      encounterId: '89284',
-      selections: [
-        'ServiceRequest/example-MRI-59879846',
-      ],
+      userId,
+      patientId,
+      encounterId,
+      selections: [`ServiceRequest/example-${patientId}`],
       draftOrders: {
         resourceType: 'Bundle',
-        entry: [{
-          resource: {
-            resourceType: 'ServiceRequest',
-            id: 'example-MRI-59879846',
-            status: 'draft',
-            intent: 'plan',
-            code: {
-              coding: [{
-                system: 'http://www.ama-assn.org/go/cpt',
-                code: '72133',
-              }],
-              text: 'Lumbar spine CT',
-            },
-            subject: {
-              reference: 'Patient/MRI-59879846',
-            },
-            reasonCode: [{
-              coding: [{
-                system: 'http://snomed.info/sct',
-                code: '279039007',
-                display: 'Low back pain',
-              }],
-            }],
-          },
-        }],
+        entry: getResources(bundle, patientId),
       },
     },
-  },
+  };
+}
+
+module.exports = {
+  s1r1: getRequest([{
+    code: '72133',
+    text: 'Lumbar spine CT',
+    snomed: [{
+      reasons: [
+        { code: '279039007', display: 'Low back pain' },
+      ],
+    }],
+  }]),
+
+  s1r2: getRequest([{
+    code: '70450',
+    text: 'CT head without contrast',
+    snomed: [{
+      reasons: [
+        { code: '25064002', display: 'Headache' },
+        { code: '423341008', display: 'Optic disc edema' },
+      ],
+    }],
+  }]),
+
+  s1r3: getRequest([{
+    code: '70544',
+    text: 'Magnetic resonance angiography, head',
+    snomed: [{
+      reasons: [
+        { code: '27355003', display: 'Toothache (finding)' },
+      ],
+    }],
+  }]),
 };
