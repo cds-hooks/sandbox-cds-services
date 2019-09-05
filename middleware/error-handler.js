a59d615d205fae4af95a61e1ecfab33ca632bd27
Josh Mandel <jmandel@med.harvard.edu>
2019-09-05 11:19:27 -0500

    Add middleware cli logging

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,6 +2,7 @@
 
 module.exports = (err, request, response, next) => {
   const status = err.status || 500;
+  console.log("Returning 500", err);
   response.status(status);
   response.set('Content-Type', 'text/html');
   response.send(status !== 500 ? err.message : 'Internal Server Error');
